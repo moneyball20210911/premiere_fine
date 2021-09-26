3cc311d554340513fcc116f3197e6cb38e714d75
unknown <dobrodobro2021@gmail.com>
2021-09-26 18:26:11 +1000

    'v-7.1'

---
+++ build/precache-manifest.182851aa3368e47c37df92f356cc1bdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298967f5ea9165907e3714bb5856440a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb4e4d3daa53ee93210",
+    "url": "/static/css/main.fe726626.chunk.css"
+  },
+  {
+    "revision": "9f34cc2366e1bd6aa6da",
+    "url": "/static/js/2.6907cc21.chunk.js"
+  },
+  {
+    "revision": "3fb4e4d3daa53ee93210",
+    "url": "/static/js/main.9c5648f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
